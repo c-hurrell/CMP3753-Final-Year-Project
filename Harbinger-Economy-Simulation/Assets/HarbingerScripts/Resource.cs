c882e71b8986643d9caee10188d288eec33ebb16
c-hurrell <charliehurrell2805@gmail.com>
2023-12-13 03:01:30 +0000

    Created Unity Project

---
+++ Harbinger-Economy-Simulation/Assets/HarbingerScripts/Resource.cs
@@ -0,0 +1,19 @@
+
+namespace HarbingerScripts
+{
+    public class Resource
+    {
+        public int id;
+        public string name;
+        public float baseVal;
+        public int supply;
+        public int demand;
+
+        public float currentValue;
+
+        public void UpdateResourceValue()
+        {
+            
+        }
+    }
+}
